472ab1836803c7ec3f3d20bc99773fd15dfe3228
Febi <febi.chajadi@usask.ca>
2022-07-28 23:32:39 -0600

    Added navigation bar code.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor} from './pages';
 
+import {useStateContext} from './contexts/ContextProvider';
 import './App.css';
 import { Side } from '@syncfusion/ej2/svg-base';
 
 const App = () => {
-    const activeMenu = true;
+  const {activeMenu} = useStateContext();
+  
   return (
     <div>
         <BrowserRouter>
@@ -35,27 +37,27 @@ const App = () => {
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                         <Navbar />
                     </div>
-                </div>
-                <div>
-                    <Routes>
-                        <Route path='/' element={<Ecommerce />} />
-                        <Route path='/ecommerce' element={<Ecommerce />} />
-                        <Route path='/orders' element={<Orders />} />
-                        <Route path='/employees' element={<Employees />} />
-                        <Route path='/customer' element={<Customers />} />
-                        <Route path='/kanban' element={<Kanban />} />
-                        <Route path='/editor' element={<Editor />} />
-                        <Route path='/calendar' element={<Calendar />} />
-                        <Route path='color-picker' element={<ColorPicker />} />
-                        <Route path='/line' element={<Line />} />
-                        <Route path='/area' element={<Area />} />
-                        <Route path='/bar' element={<Bar />} />
-                        <Route path='/pie' element={<Pie />} />
-                        <Route path='/financial' element={<Financial />} />
-                        <Route path='/color-mapping' element={<ColorMapping />} />
-                        <Route path='/pyramid' element={<Pyramid />} />
-                        <Route path='/stack' element={<Stacked />} />
-                    </Routes>
+                    <div>
+                        <Routes>
+                            <Route path='/' element={<Ecommerce />} />
+                            <Route path='/ecommerce' element={<Ecommerce />} />
+                            <Route path='/orders' element={<Orders />} />
+                            <Route path='/employees' element={<Employees />} />
+                            <Route path='/customer' element={<Customers />} />
+                            <Route path='/kanban' element={<Kanban />} />
+                            <Route path='/editor' element={<Editor />} />
+                            <Route path='/calendar' element={<Calendar />} />
+                            <Route path='color-picker' element={<ColorPicker />} />
+                            <Route path='/line' element={<Line />} />
+                            <Route path='/area' element={<Area />} />
+                            <Route path='/bar' element={<Bar />} />
+                            <Route path='/pie' element={<Pie />} />
+                            <Route path='/financial' element={<Financial />} />
+                            <Route path='/color-mapping' element={<ColorMapping />} />
+                            <Route path='/pyramid' element={<Pyramid />} />
+                            <Route path='/stack' element={<Stacked />} />
+                        </Routes>
+                    </div>
                 </div>
             </div>
         </BrowserRouter>
